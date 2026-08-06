d668b494f35f280bc163c92098a1fe80b3221a1e
Muhammad Hammad <smuhammadhammad005@gmail.com>
2026-08-06 18:06:46 +0500

    c

--- lib/data/repository/stock_repository.dart
+++ lib/data/repository/stock_repository.dart
@@ -1,49 +1,25 @@
 import 'dart:async';
-
 import '../models/stock.dart';
 import '../services/api_service.dart';
 import '../services/stock_data_source.dart';
 
-
 class StockRepository {
-
   final ApiService apiService;
-
   StockDataSource? dataSource;
-
-
   StockRepository({
     required this.apiService,
   });
 
-
   Future<List<Stock>> getStocks() async {
-
     final stocks = await apiService.fetchStocks();
-
-
     dataSource = StockDataSource(stocks);
-
-
     dataSource!.start();
-
-
     return stocks;
-
   }
-
-
   Stream<Stock> getStockStream(String symbol) {
-
     return dataSource!.getStockStream(symbol);
-
   }
-
-
   void dispose(){
-
     dataSource?.dispose();
-
   }
-
 }
